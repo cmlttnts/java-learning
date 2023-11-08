85b5c559a640b045438e6beaf69d9046088be0c4
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2023-11-08 05:42:32 +0300

    String cont.

--- app/src/main/java/basic/strings/Main.java
+++ app/src/main/java/basic/strings/Main.java
@@ -1,28 +1,36 @@
 package basic.strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
-    private static void seperate(){
+    private static void seperate() {
         System.out.println("------------------------------------");
     }
 
-    public static void stringsMain(){
+    public static void stringsMain() {
         concating();
         seperate();
-        
+
         multiLine();
         seperate();
-        
+
         joining();
         seperate();
 
         stringBufferAndBuilder();
         seperate();
+
+        emptyStrings();
+        seperate();
+
+        stringSplit();
+        seperate();
     }
 
-    private static void concating(){
+    private static void concating() {
         String str1 = "Hello";
         String str2 = "World!1";
 
@@ -33,28 +41,28 @@ public class Main {
 
     private static void multiLine() {
         String multi1 = """
-            Hello World!1
-                Next Line
-            """;
+                Hello World!1
+                    Next Line
+                """;
 
         System.out.println(multi1);
 
-        String multi2 ="""
-            Hello World!2
-            Next Line
-        """;
+        String multi2 = """
+                    Hello World!2
+                    Next Line
+                """;
         System.out.println(multi2);
 
         String multi3 = """
-            Hello World!3
-            Next Line""";
+                Hello World!3
+                Next Line""";
 
         System.out.println(multi3);
     }
-    
-    private static void joining(){
+
+    private static void joining() {
         System.out.println(String.join("-", "1", "2", "3"));
-        String[] strings = { "one", "two", "three"};
+        String[] strings = { "one", "two", "three" };
         System.out.println(String.join("-", strings));
         ArrayList<String> strings2 = new ArrayList<>();
         strings2.add("111");
@@ -67,8 +75,7 @@ public class Main {
         System.out.println(String.join("-", linkedStrs));
     }
 
-
-    private static void stringBufferAndBuilder(){
+    private static void stringBufferAndBuilder() {
         StringBuffer sb = new StringBuffer("String");
         sb.append(" ");
         sb.append("Buffer!");
@@ -79,5 +86,38 @@ public class Main {
         sb2.append(" ");
         sb2.append("Builder");
         System.out.println(sb2);
+    }
+
+    private static void emptyStrings() {
+        String empty = "";
+        System.out.println(empty.isEmpty());
+        System.out.println(empty.isBlank());
+
+        String empty2 = " \t\n";
+        System.out.println(empty2.isEmpty());
+        System.out.println(empty2.isBlank());
+
+        String trimmed = empty2.trim();
+        System.out.println(trimmed.isEmpty());
+        System.out.println(trimmed.isBlank());
+    }
+
+    private static void stringSplit() {
+        String commaSeperated = "1,2,3,4,5";
+        String[] splittedArray = commaSeperated.split(",");
+
+        for (String str : splittedArray) {
+            System.out.println(str);
+        }
+
+        // default split
+        String str2 = "Hello World 1 2 3   extraSpaces";
+        String[] splitted = str2.split(" ");
+        System.out.println(Arrays.toString(splitted));
+
+        for (String str : splitted) {
+            System.out.println(str);
+        }
+
     }
 }
